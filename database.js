09643c90bd20de522b1c1571f964ca3c39c3327d
metiuLOL <105188953+metiuLOL@users.noreply.github.com>
2022-05-09 12:05:16 +0700

    first upload

---
+++ database.js
@@ -0,0 +1,117 @@
+let Hp = [];
+
+let informasi = {
+    Foto:"https://cdn1.katadata.co.id/media/images/thumb/2021/10/28/iPhone_13_Pro-2021_10_28-14_49_09_14b4d070187b2b687fe2affe100d8934_400x267_thumb.png",
+    Harga:22000000,
+    Kode:"BRG001",
+    Nama:"iphone 11 promax",
+    Rating:"5.0",
+    Stock:10,  
+}
+Hp.push(informasi);
+ let informasi2 = {
+    Foto:"https://images.tokopedia.net/img/cache/500-square/product-1/2018/10/23/3890415/3890415_a539dbfb-ad4b-473b-9c32-c93a874591ed_700_700.jpg",
+    Harga:5000000,
+    Kode:"BRG002",
+    Nama:"Samsung A7 Lite",
+    Rating:"5.0",
+    Stock:20,  
+}
+
+Hp.push(informasi2);
+let informasi3 = {
+    Foto:"https://assets.pikiran-rakyat.com/crop/0x0:0x0/x/photo/2022/03/04/32727645.png",
+    Harga:7000000,
+    Kode:"BRG003",
+    Nama:"Oppo Reno 9",
+    Rating:"5.0",
+    Stock: 100,
+
+}
+
+    Hp.push(informasi3);
+    let informasi4 = {
+    Foto:"https://i01.appmifile.com/webfile/globalimg/id/cms/94E6B0C6-6C8A-0DE1-F7EF-CA1EC88EDAD6.jpg",
+    Harga:5000000,
+    Kode:"BRG004",
+    Nama:"Xiomi Redmi Note 11",
+    Rating:"5.0",
+    Stock:100,
+    
+
+    }
+
+    Hp.push(informasi4);
+   let informasi5 = {
+    Foto:"https://asia-exstatic-vivofs.vivo.com/PSee2l50xoirPK7y/1643369451893/1a8170069de201d196abee9e29c01e49.png",
+    Harga:6000000,
+    Kode:"BRG005",
+    Nama:"Vivo",
+    Rating:"5.0",
+    Stock:50,
+   
+
+    }
+
+    Hp.push(informasi5);
+   let informasi6 = {
+    Foto:"http://p-id.ipricegroup.com/uploaded_3bab1cf4a8a52d9b8b3497501c698db0.jpg",
+    Harga:6000000,
+    Kode:"BRG006",
+    Nama:"Huawei P50",
+    Rating:"5.0",
+    Stock:100,
+   
+
+    }
+
+    Hp.push(informasi6);
+    let informasi7 = {
+    Foto:"https://awsimages.detik.net.id/community/media/visual/2022/01/04/samsung-galaxy-a03s-1.png?w=1200",
+    Harga:5000000,
+    Kode:"BRG007",
+    Nama:"Samsung",
+    Rating:"5.0",
+    Stock:50,
+   
+
+    }
+
+    Hp.push(informasi7);
+   let informasi8 = {
+    Foto:"https://id-test-11.slatic.net/p/1f8e9bb70e50a95b21d3f2ea9ad29e39.jpg",
+    Harga:6000000,
+    Kode:"BRG008",
+    Nama:"Evercross",
+    Rating:"3.5",
+    Stock:120,
+   
+
+    }
+
+    Hp.push(informasi8);
+   let informasi9 = {
+    Foto:"https://cdn.eraspace.com/pub/media/catalog/product/a/p/apple_iphone_11_pro_midnight_green_new_1_2.jpg",
+    Harga:20000000,
+    Kode:"BRG009",
+    Nama:"iPhone 11 Pro",
+    Rating:"5.0",
+    Stock:50,
+   
+
+    }
+
+    
+    Hp.push(informasi9);
+   let informasi10 = {
+    Foto:"https://i01.appmifile.com/webfile/globalimg/id/cms/8B7E49CE-95D0-A8CA-5346-2D2DA359B2DE.jpg",
+    Harga:6000000,
+    Kode:"BRG0010",
+    Nama:"Redmi 10",
+    Rating:"5.0",
+    Stock:100,
+   
+
+    }
+Hp.push(informasi10);
+console.log(Hp);
